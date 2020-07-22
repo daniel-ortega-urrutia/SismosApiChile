53d0a7ed0998da2f0deb4bd3597db1dc99efa15c
Daniel Ortega Urrutia <danielduoc14@gmail.com>
2020-07-21 23:51:46 -0400

    se agrego un menu, con ciclo while a la rama menu

--- src/main/java/cl/desafiolatam/App.java
+++ src/main/java/cl/desafiolatam/App.java
@@ -9,16 +9,35 @@ import java.util.Scanner;
  */
 public class App {
 	public static void main(String[] args) {
-
 		Sismos sismos = new Sismos();// instancio la clase
 		// creo una nueva lista, le entrego el metodo que me retorna una lista con los
 		// sismos
 		List<Sismos> listaSismos = sismos.retornarSismos();
-		System.out.println(listaSismos);// imprimo los sismos
-		sismos.mostrarUltimoSismo(listaSismos);// muestra el ultimo sismo en chile
-		
-		System.out.println("Creacion de nuevo menu: ");
 		Scanner sc = new Scanner(System.in);
+		String opcion = "";
+		System.out.println("Bienvenido: \n¿Que desea hacer?");
+		while (!opcion.equals("salir")) {
+			System.out.printf("1-Listar ultimo sismo \n");
+			System.out.printf("2-Listar todos los sismos \n");
+			System.out.printf("escriba salir para terminar el programa \n");
+			opcion = sc.nextLine();
+
+			if (opcion.equalsIgnoreCase("1")) {
+				sismos.mostrarUltimoSismo(listaSismos);
+
+			} else if (opcion.equalsIgnoreCase("2")) {
+				System.out.println(listaSismos);
+			} else if (opcion.equalsIgnoreCase("salir")) {
+				System.out.println("Saliendo... \n");
+//			cerramos el programa
+				System.exit(0);
+
+			} else {
+				System.out.println("Opcion invalida \n");
+			}
+		}
+		// termina metodo main
 
 	}
+
 }
